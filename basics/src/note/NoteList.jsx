baed6192b904a0719ffdd2575fa9eb6ea6ac069c
nuhptr <nugrahaadi733@gmail.com>
2025-03-16 22:07:28 +0700

    feat: :rocket:
    update note list to useMemo()

--- basics/src/note/NoteList.jsx
+++ basics/src/note/NoteList.jsx
@@ -1,4 +1,4 @@
-import { use } from "react"
+import { use, useMemo, useRef, useState } from "react"
 
 import Note from "./Note"
 import { NotesContext } from "./NoteContext"
@@ -6,9 +6,26 @@ import { NotesContext } from "./NoteContext"
 export function NoteList() {
     const notes = use(NotesContext)
 
+    // TODO: Setup state and ref
+    const [search, setSearch] = useState("")
+    const searchInput = useRef(null)
+
+    // TODO: Implementation of useMemo()
+    const filteredNotes = useMemo(() => {
+        console.log("Filtering notes...")
+        return notes.filter((note) => note.text.includes(search))
+    }, [notes, search])
+
+    const handleSearch = () => {
+        console.log("Searching...", searchInput.current.value)
+        setSearch(searchInput.current.value)
+    }
+
     return (
         <ul>
-            {notes.map((note) => (
+            <input type="text" ref={searchInput} placeholder="Search" />
+            <button onClick={handleSearch}>Search</button>
+            {filteredNotes.map((note) => (
                 <li key={note.id}>
                     <Note note={note} />
                 </li>
