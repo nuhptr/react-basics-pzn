f91f41c0b84e666efab62833a0de2203406698e5
nuhptr <nugrahaadi733@gmail.com>
2025-03-16 19:34:04 +0700

    refactor: :hammer: reset state

--- basics/src/counter/CounterApp.jsx
+++ basics/src/counter/CounterApp.jsx
@@ -11,7 +11,12 @@ export default function CounterApp() {
     return (
         <div>
             {/* State dipertahankan gara-gara posisinya sama */}
-            {show2 ? <Counter name="Budi" /> : <p>Hilang</p>}
+            {/* {show2 ? <Counter name="Budi" /> : <p>Hilang</p>} */}
+            {/* Reset state */}
+            {show2 ? <Counter key="budi" name="Budi" /> : <Counter key="eko" name="Eko" />}
+            {/* Beda posisi, state tidak dipertahankan */}
+            {/* {show2 && <Counter name="Budi" />} */}
+            {/* {!show2 && <Counter name="Eko" />} */}
             <input type="checkbox" checked={show2} onChange={handleChange} /> Tampilkan 2 Counter
         </div>
     )
